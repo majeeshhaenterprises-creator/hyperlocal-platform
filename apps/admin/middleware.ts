a015506b0c067f92b85de3f94fa729d47f542ad8
Jay Soni <majeeshhaenterprises@gmail.com>
2026-06-17 02:43:15 +0530

    Fix Vercel middleware

--- apps/admin/middleware.ts
+++ apps/admin/middleware.ts
@@ -2,13 +2,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const isLoggedIn =
-    request.cookies.get("admin_logged_in");
+  const token = request.cookies.get("admin_logged_in")?.value;
 
-  const isAdminRoute =
-    request.nextUrl.pathname.startsWith("/admin");
-
-  if (isAdminRoute && !isLoggedIn) {
+  if (
+    request.nextUrl.pathname.startsWith("/admin") &&
+    token !== "true"
+  ) {
     return NextResponse.redirect(
       new URL("/auth/login", request.url)
     );
